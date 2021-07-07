38c4f54b28a01b52168ad08d7f1a83ba50686b82
anggidast <adastariana@gmail.com>
2021-07-08 01:09:15 +0700

    development init

---
+++ client/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>tictactoe</h1>
+  </div>
+</template>
+
+<script>
+import {io} from 'socket.io-client'
+const socket = io('http://localhost:3000')
+
+export default {
+  name: 'App',
+  mounted() {
+    socket.on('connect', () => {
+      console.log('connected with id: ', socket.id);
+    })
+  }
+}
+</script>
+
+<style>
+
+</style>
